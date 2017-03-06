c09e7a464fa01599b18b4167b1528e5123d704c2
stites <sam@stites.io>
2017-03-05 19:16:41 -0500

    add bk

--- exe/Bk.hs
+++ exe/Bk.hs
@@ -1,8 +1,39 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc --package turtle
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveAnyClass #-}
 module Main where
 
-import System.Directory
+import Prelude hiding (FilePath)
 import Turtle
+import Control.Monad.Catch
+
 
 main :: IO ()
-main = undefined
+main = do
+  fp <- options "toggle the `.bk` extension on a file" parser
+  checkFile fp
+  mv fp (moveTo fp)
+
+
+parser :: Parser FilePath
+parser = argPath "file" "The file to change extensions of"
+
+
+moveTo :: FilePath -> FilePath
+moveTo fp =
+ if hasExtension fp "bk"
+ then dropExtension fp
+ else fp <.> "bk"
+
+
+checkFile :: FilePath -> IO ()
+checkFile fp = do
+  valid <- testfile fp
+  unless valid (throwM FileDoesNotExist)
+
+
+data BkExceptions = FileDoesNotExist
+  deriving (Show, Exception)
+
 
